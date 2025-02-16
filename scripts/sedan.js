4ae17e97e57b0a71d394c18fd8c63bc4dd90e58c
Cha Hajin <rltk4545@gmail.com>
2025-02-16 21:56:29 +0800

    add legend

--- scripts/sedan.js
+++ scripts/sedan.js
@@ -18,10 +18,10 @@ window.onload = function() {
     }
 
     document.getElementById('tesla').addEventListener('click', function() {
-        window.location.href = buildURL('http://localhost:8080/reservation.html', 'Tesla Model 3', '160.00', 'assets/images/tesla_model_3_front.png');
+        window.location.href = buildURL('reservation.html', 'Tesla Model 3', '160.00', 'assets/images/tesla_model_3_front.png');
     });
 
     document.getElementById('byd').addEventListener('click', function() {
-        window.location.href = buildURL('http://localhost:8080/reservation.html', 'BYD Seal', '128.00', 'assets/images/byd_seal_front.png');
+        window.location.href = buildURL('reservation.html', 'BYD Seal', '128.00', 'assets/images/byd_seal_front.png');
     });
 };
